5ee8a3cb475a5d0e6b511b009c80f1049a13a4be
Scott Driscoll <scott.driscoll08@gmail.com>
2025-04-19 17:24:27 -0700

    Start memory system, add phpunit

---
+++ src/Command/Chips8Command.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Command;
+
+use App\Systems\Memory;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+
+#[AsCommand(
+    name: 'app:chips8',
+    description: 'Add a short description for your command',
+)]
+class Chips8Command extends Command
+{
+    public function __construct(private readonly Memory $memory)
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+
+        $this->memory->loadRom(__DIR__ . '/../../tests/fixtures/roms/ibm_logo.ch8');
+
+        $io = new SymfonyStyle($input, $output);
+        $arg1 = $input->getArgument('arg1');
+
+        if ($arg1) {
+            $io->note(sprintf('You passed an argument: %s', $arg1));
+        }
+
+        if ($input->getOption('option1')) {
+            // ...
+        }
+
+        $io->success('You have a new command! Now make it your own! Pass --help to see your options.');
+
+        return Command::SUCCESS;
+    }
+}
---
+++ src/Systems/Memory.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Systems;
+
+use Symfony\Contracts\Service\Attribute\Required;
+
+class Memory
+{
+    const ROM_START = 512;
+    const MEMORY_SIZE = 4096;
+
+    /**
+     * 4096 bytes
+     * @var array
+     */
+    private array $memory = [];
+
+    public function __construct()
+    {
+
+    }
+
+    /**
+     * Loads a ROM into memory.
+     *
+     * The first 512 bytes are reserved for fonts. The rest is for the ROM.
+     * Memory is considered read and writable, and is always exactly 4096 bytes.
+     */
+    public function loadRom(string $path): void
+    {
+        $hex = unpack("H*", file_get_contents($path));
+        $hex = current($hex);
+
+        $this->memory = array_fill(0, self::ROM_START, null);
+
+        foreach (str_split($hex, 2) as $byte) {
+            $this->memory[] = $byte;
+        }
+
+        $this->memory = array_pad($this->memory, self::MEMORY_SIZE, null);
+    }
+
+    public function getMemory(): array
+    {
+        return $this->memory;
+    }
+}
